1bc3fe0f9942641338466351d11a07e23578653e
BrendaOliveira-code <61034227+BrendaOliveira-code@users.noreply.github.com>
2020-04-17 20:06:41 -0300

    Alterando os codigos, igual ao do professor.

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,6 @@
-# co# content of test_sample.py
+# content of test_sample.py
 def func(x):
     return x + 1
 
-
 def test_answer():
-    assert func(3) == 5
+    assert func(3) == 4, 'Deveria ser 4'
